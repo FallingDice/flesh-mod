935499db3a314ec5cf3490a5af5cc7e74c66de95
FallingDice <songinsuak@gmail.com>
2024-07-25 16:42:46 +0900

    Delete scripts/units directory

--- scripts/units/fleshball.js
+++
@@ -1,5 +0,0 @@
-const spawnUnit = Vars.content.getByName(ContentType.unit, "fleshball");
-
-unitSpawn1.spawnEffect = mendSpawn;
-
-fleshball.abilities.add(new UnitSpawnAbility(spawnUnit, 7 * 60, 18, -7));
