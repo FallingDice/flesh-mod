f812f11612c4f0d63cd8e8d0514f5f6de7b4684f
FallingDice <songinsuak@gmail.com>
2024-10-25 12:25:06 +0900

    Update and rename testunit2f.js to testunit1f.js

---
+++ scripts/units/testunit1f.js
@@ -0,0 +1,35 @@
+// 새로운 유닛 정의
+const cunit = extend(UnitType, "testunit1f", {});
+cunit.constructor = () => extend(UnitEntity, {});
+
+// 유닛의 무기 정의
+const gun1 = extend(Weapon, {
+  y: 5,
+  shots: 1,
+  x: 5,
+  top: true,
+  rotate: true,
+  inaccuracy: 2,
+  reload: 10,
+  alternate: true,
+  shootSound: Sounds.pew,
+  bullet: testbul
+});
+
+const testbul = extend(BasicBulletType, {
+  speed: 8,
+  damage: 15,
+  width: 7,
+  height: 9,
+  frontColor: Color.valueOf("#6ecdec"),
+  backColor: Color.valueOf("#5091a6"),
+  status: StatusEffects.freezing,
+  lifetime: 32,
+  buildingDamageMultiplier: 0.3,
+  keepVelocity: false
+});
+
+
+cunit.weapons.add(
+  gun1
+);
