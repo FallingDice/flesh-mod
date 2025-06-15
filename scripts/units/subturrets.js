25d83080ab2ff2dc7c3b9cb54a07bfcf38556da9
FallingDice <songinsuak@gmail.com>
2025-06-15 23:16:52 +0900

    Update subturrets.js

--- scripts/units/subturrets.js
+++ scripts/units/subturrets.js
@@ -23,38 +23,20 @@ const hive = extend(PowerTurret, "hive", {
   destructible: true,
   rebuildable: false,
   destroySound: Sounds.plantBreak,
-  update: true  // 반드시 true로 해야 update()가 호출됩니다
+  update: false  // 동작 X
 });
 
-// 1. 총알 타입 정의
-const hiveBullet = extend(BasicBulletType, {
-  speed: 6,
-  damage: 15,
-  lifetime: 50,
-  width: 8,
-  height: 8,
-  hitEffect: Fx.hitBullet,
-  hitSound: Sounds.hitBullet,
-});
-
-// 2. 무기 정의
-const hiveWeapon = extend(Weapon, {
-  reload: 30,
-  recoil: 1,
-  shootSound: Sounds.shoot,
-  bullet: hiveBullet,
-  x: 0,
-  y: 0,
-  top: true,
-});
-
-// 3. 무기 등록
-hive.weapons.add(hiveWeapon);
-
-// 4. buildType에 update 구현, 반드시 super$update() 호출
 hive.buildType = () => extend(PowerTurret.PowerTurretBuild, hive, {
-  update() {
-    this.super$update();
+  placed() {
+    this.super$placed();
+    // 설치될 때 소환할 유닛 지정
+    const unitType = Vars.content.getByName(ContentType.unit, "fleshball");
+    if(unitType){
+      // 설치 위치 근처에 소환
+      let spawnX = this.x;
+      let spawnY = this.y;
+      unitType.spawn(this.team, spawnX, spawnY);
+    }
   }
 });
 
