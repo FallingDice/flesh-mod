c77aff2cadaf0de485c94241705d017bb06e8398
FallingDice <songinsuak@gmail.com>
2025-06-15 23:33:49 +0900

    Update subturrets.js

--- scripts/units/subturrets.js
+++ scripts/units/subturrets.js
@@ -17,6 +17,7 @@ module.exports = {
 
 
 
+/*
 const hive = extend(PowerTurret, "hive", {
   size: 3,
   solid: false,
@@ -34,7 +35,7 @@ const spawnMissile = extend(BasicBulletType, {
   collidesAir: false,
   instanceDisappear: true, // 핵심!
   despawned(b){
-    const unitType = Vars.content.getByName(ContentType.unit, "dagger");
+    const unitType = Vars.content.getByName(ContentType.unit, "fleshball");
     if(unitType){
       unitType.spawn(b.team, b.x, b.y);
     }
@@ -45,6 +46,48 @@ hive.buildType = () => extend(PowerTurret.PowerTurretBuild, hive, {
   placed() {
     this.super$placed();
     spawnMissile.create(this, this.team, this.x, this.y, 0, 0, 0);
+  }
+});
+
+module.exports = {
+  hive: hive
+};
+*/
+
+
+
+
+const hive = extend(PowerTurret, "hive", {
+  size: 3,
+  solid: false,
+  destructible: true,
+  rebuildable: false,
+  destroySound: Sounds.plantBreak,
+  update: false
+});
+
+const spawnMissile = extend(BasicBulletType, {
+  speed: 0,
+  lifetime: 1,
+  damage: 0,
+  hitEffect: Fx.none,
+  despawnEffect: Fx.none,
+  collides: false,
+  collidesAir: false,
+  instanceDisappear: true,  // 필수
+  keepVelocity: false,
+  despawned(b) {
+    const unitType = Vars.content.getByName(ContentType.unit, "fleshball");
+    if (unitType != null) {
+      unitType.spawn(b.owner.team, b.x, b.y);
+    }
+  }
+});
+
+hive.buildType = () => extend(PowerTurret.PowerTurretBuild, hive, {
+  placed() {
+    this.super$placed();
+    spawnMissile.create(this, this.team, this.x, this.y, 0, 0, 1, 1);
   }
 });
 
