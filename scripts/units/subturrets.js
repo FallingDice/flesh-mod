1a6b69a06ec1aa562a00fffc332453acd7ddfed0
FallingDice <songinsuak@gmail.com>
2025-06-15 23:40:44 +0900

    u

--- scripts/units/subturrets.js
+++ scripts/units/subturrets.js
@@ -1,7 +1,4 @@
-/*
 const hive = extend(PowerTurret, "hive", {
-  size: 3,
-  solid: false,
   destructible: true,
   rebuildable: false,
   destroySound: Sounds.plantBreak,
@@ -9,87 +6,6 @@ const hive = extend(PowerTurret, "hive", {
 });
 
 hive.buildType = () => extend(PowerTurret.PowerTurretBuild, hive, {});
-
-module.exports = {
-  hive: hive
-};
-*/
-
-
-
-/*
-const hive = extend(PowerTurret, "hive", {
-  size: 3,
-  solid: false,
-  destructible: true,
-  rebuildable: false,
-  destroySound: Sounds.plantBreak,
-  update: false
-});
-
-const spawnMissile = extend(BasicBulletType, {
-  damage: 0,
-  speed: 0,
-  lifetime: 0,
-  collides: false,
-  collidesAir: false,
-  instanceDisappear: true, // 핵심!
-  despawned(b){
-    const unitType = Vars.content.getByName(ContentType.unit, "fleshball");
-    if(unitType){
-      unitType.spawn(b.team, b.x, b.y);
-    }
-  }
-});
-
-hive.buildType = () => extend(PowerTurret.PowerTurretBuild, hive, {
-  placed() {
-    this.super$placed();
-    spawnMissile.create(this, this.team, this.x, this.y, 0, 0, 0);
-  }
-});
-
-module.exports = {
-  hive: hive
-};
-*/
-
-
-
-
-const hive = extend(PowerTurret, "hive", {
-  size: 3,
-  solid: false,
-  destructible: true,
-  rebuildable: false,
-  destroySound: Sounds.plantBreak,
-  update: false
-});
-
-const spawnMissile = extend(BasicBulletType, {
-  speed: 0,
-  lifetime: 1,
-  damage: 0,
-  hitEffect: Fx.none,
-  despawnEffect: Fx.none,
-  collides: false,
-  collidesAir: false,
-  instanceDisappear: true,  // 필수
-  keepVelocity: false,
-  despawned(b) {
-    const unitType = Vars.content.getByName(ContentType.unit, "fleshball");
-    if (unitType != null) {
-      unitType.spawn(b.owner.team, b.x, b.y);
-    }
-  }
-});
-
-hive.buildType = () => extend(PowerTurret.PowerTurretBuild, hive, {
-  placed() {
-    this.super$placed();
-    spawnMissile.create(this, this.team, this.x, this.y, 0, 0, 1, 1);
-  }
-});
 
 module.exports = {
   hive: hive
