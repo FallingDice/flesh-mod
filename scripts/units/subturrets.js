339ecbce99c63f987c6bcd1da14aff2aeda716b6
FallingDice <songinsuak@gmail.com>
2025-06-15 23:57:49 +0900

    u

--- scripts/units/subturrets.js
+++ scripts/units/subturrets.js
@@ -1,8 +1,29 @@
+/*
 const hive = extend(PowerTurret, "hive", {
+  size: 3,
+  solid: false,
   destructible: true,
   rebuildable: false,
   destroySound: Sounds.plantBreak,
   update: false  // 동작 X
+});
+
+hive.buildType = () => extend(PowerTurret.PowerTurretBuild, hive, {});
+
+module.exports = {
+  hive: hive
+};
+*/
+
+
+
+const hive = extend(PowerTurret, "hive", {
+  size: 3,
+  solid: false,
+  destructible: true,
+  rebuildable: false,
+  destroySound: Sounds.plantBreak,
+  update: true
 });
 
 hive.buildType = () => extend(PowerTurret.PowerTurretBuild, hive, {});
